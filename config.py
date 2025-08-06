c194c215c2b885d5b71b045d4d02b7d70d4ad672
dangtoi05122003 <dangtoi05122003@gmail.com>
2025-08-07 00:48:59 +0700

    Fix

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ def setup():
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920,1080')
+    options.add_argument("--remote-debugging-port=0")
     return options
