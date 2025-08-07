8f18b07e52d963258a4ebfd07a1ac33d10931215
dangtoi05122003 <dangtoi05122003@gmail.com>
2025-08-07 13:29:40 +0700

    Fix

--- index.py
+++ index.py
@@ -18,16 +18,13 @@ class main(webdriver.Chrome):
 
 
         #self.maximize_window()
-        sleep(5)
     def website(self):
-        self.set_page_load_timeout(60)
         self.get("https://e.vnexpress.net/")
     def database(self):
         client = MongoClient(MONGO_URI)
         db = client[MONGO_DB]
         self.collection = db[MONGO_COLLECTION]
     def trend(self):
-        self.implicitly_wait(15)
         Col_topnews = self.find_element(By.CSS_SELECTOR, '.col-left-topstory.flexbox')
         item_topnews = Col_topnews.find_elements(By.CLASS_NAME, 'item-topstory')
         for item in item_topnews:
